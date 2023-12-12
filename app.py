4ddc50dfe4653abaeb5168c50e16bb1408cfcfe7
Flaviovinicius2410 <114126342+Flaviovinicius2410@users.noreply.github.com>
2023-12-12 16:55:07 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,15 +7,14 @@ df = pd.read_csv("train.csv")
 # Imprima as colunas do DataFrame
 st.write("Colunas no DataFrame:", df.columns)
 
-st.write("Conteúdo do DataFrame:", df.head())
 # Informações sobre jogadores
 st.subheader("Informações sobre Jogadores:")
 st.write(df[['player_id','name', 'nationality', 'position', 'overall', 'age', 'hits', 'potential', 'team']])
 
 # Gráfico de contagem de jogadores por posição
 st.subheader("Contagem de Jogadores por Posição:")
-position_counts = df['position'].str.split('|', expand=True).stack().value_counts()
-st.bar_chart(position_counts)  # Corrigido para st.bar_chart
+position_counts = df['position'].explode().value_counts()
+st.bar_chart(position_counts)
 
 # Gráfico de distribuição de idades
 st.subheader("Distribuição de Idades:")
