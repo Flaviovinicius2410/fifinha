4278e3039e3c66fe449b18f0c533f5c2119e237f
Flaviovinicius2410 <114126342+Flaviovinicius2410@users.noreply.github.com>
2023-12-12 18:05:36 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -17,18 +17,15 @@ st.write(df[['player_id', 'name', 'nationality', 'position', 'overall', 'age', '
 pure_positions = df['position'].str.split('|', expand=True).stack().value_counts()
 
 # Gráfico de distribuição de jogadores por posição pura em um gráfico de barras
-st.subheader("Contagem de Jogadores por Posição Pura:")
+st.subheader("Jogadores Por Posições:")
 fig, ax = plt.subplots()
 pure_positions.sort_values().plot(kind='barh', ax=ax, color='skyblue')
 ax.set_xlabel('Contagem de Jogadores')
-ax.set_ylabel('Posição Pura')
+ax.set_ylabel('Posição Geral')
 ax.set_title('Posições dos jogadores')
 st.pyplot(fig)
 
-# Gráfico de distribuição de idades
-st.subheader("Distribuição de Idades:")
-st.hist(df['age'], bins=20, edgecolor='black')
-st.xlabel('Idade')
-st.ylabel('Contagem')
-st.title('Distribuição de Idades dos Jogadores')
-st.pyplot()
+# Adicionando tabela dos 50 melhores jogadores
+st.subheader("Top 50 Jogadores:")
+top_50_players = df[['name', 'overall']].nlargest(50, 'overall')
+st.table(top_50_players.set_index('name'))
