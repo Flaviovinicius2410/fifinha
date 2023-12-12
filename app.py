ad11d48559b3c8cca6a794743ebea8d917a6f880
Flaviovinicius2410 <114126342+Flaviovinicius2410@users.noreply.github.com>
2023-12-12 18:38:50 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -30,8 +30,12 @@ st.subheader("Top 50 Jogadores - Gráfico de Barras:")
 # Adicionando critério de desempate usando idade, potencial e hits
 top_50_players = df[['name', 'overall', 'age', 'potential', 'hits']].nlargest(50, ['overall', 'age', 'potential', 'hits'])
 fig, ax = plt.subplots()
-ax.barh(top_50_players['name'], top_50_players['overall'], color='green')
-ax.set_xlabel('Overall')
+bar_width = 0.5
+ax.barh(top_50_players['name'], top_50_players['overall'], color='green', height=bar_width)
+ax.barh(top_50_players['name'], top_50_players['potential'], color='orange', height=bar_width, left=top_50_players['overall'])
+ax.set_xlabel('Pontuação')
 ax.set_ylabel('Jogadores')
 ax.set_title('Top 50 Jogadores')
+ax.legend(['Overall', 'Potencial'])
+ax.invert_yaxis()  # Inverte a ordem dos jogadores
 st.pyplot(fig)
