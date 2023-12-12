7d1e71f7bda0fa9a30e0d40f75930967172f34a2
Flaviovinicius2410 <114126342+Flaviovinicius2410@users.noreply.github.com>
2023-12-12 17:01:01 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,23 +2,25 @@ import streamlit as st
 import pandas as pd
 
 # Carregando o conjunto de dados do Titanic
-df = pd.read_csv("train.csv")
+# Usando ";" como delimitador no read_csv
+df = pd.read_csv("train.csv", delimiter=";")
 
 # Imprima as colunas do DataFrame
 st.write("Colunas no DataFrame:", df.columns)
 
 # Informações sobre jogadores
 st.subheader("Informações sobre Jogadores:")
-st.write(df[['player_id','name', 'nationality', 'position', 'overall', 'age', 'hits', 'potential', 'team']])
+st.write(df[['player_id', 'name', 'nationality', 'position', 'overall', 'age', 'hits', 'potential', 'team']])
 
 # Gráfico de contagem de jogadores por posição
 st.subheader("Contagem de Jogadores por Posição:")
+# Usando explode() para lidar com valores separados por '|'
 position_counts = df['position'].explode().value_counts()
 st.bar_chart(position_counts)
 
 # Gráfico de distribuição de idades
 st.subheader("Distribuição de Idades:")
-st.hist(df['age'], bins=20, edgecolor='black')  # Corrigido para st.hist
+st.hist(df['age'], bins=20, edgecolor='black')
 st.xlabel('Idade')
 st.ylabel('Contagem')
 st.title('Distribuição de Idades dos Jogadores')
