e9266089def9b9a4746b47d019710a023d8010c1
Flaviovinicius2410 <114126342+Flaviovinicius2410@users.noreply.github.com>
2023-12-12 17:21:19 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Carregando o conjunto de dados do Titanic
 # Usando ";" como delimitador no read_csv
@@ -16,7 +17,14 @@ st.write(df[['player_id', 'name', 'nationality', 'position', 'overall', 'age', '
 st.subheader("Distribuição Percentual de Jogadores por Posição:")
 # Usando explode() para lidar com valores separados por '|'
 position_counts = df['position'].explode().value_counts(normalize=True)
-st.pie(position_counts, labels=position_counts.index, autopct='%1.1f%%', startangle=140)
+
+# Criar gráfico de pizza com Matplotlib
+fig, ax = plt.subplots()
+ax.pie(position_counts, labels=position_counts.index, autopct='%1.1f%%', startangle=140)
+ax.axis('equal')  # Assegura que o gráfico de pizza seja circular
+
+# Exibir o gráfico usando Streamlit
+st.pyplot(fig)
 
 # Gráfico de distribuição de idades
 st.subheader("Distribuição de Idades:")
