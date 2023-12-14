3ef0fda78cc67fa27a9a3b5afdc3dc16270b1361
Flaviovinicius2410 <114126342+Flaviovinicius2410@users.noreply.github.com>
2023-12-14 00:21:51 -0300

    Add files via upload

--- pages/funcionamentodabase.py
+++ pages/funcionamentodabase.py
@@ -22,20 +22,3 @@ st.write("- **age**: Idade do jogador.")
 st.write("- **hits**: Número de 'hits' ou popularidade do jogador.")
 st.write("- **potential**: Potencial futuro do jogador.")
 st.write("- **team**: Time ao qual o jogador pertence.")
-
-# Gráfico de distribuição de jogadores por posição pura em um gráfico de barras
-st.write("## Jogadores Por Posições:")
-fig, ax = plt.subplots()
-pure_positions = df['position'].str.split('|', expand=True).stack().value_counts()
-pure_positions.sort_values().plot(kind='barh', ax=ax, color='skyblue')
-ax.set_xlabel('Contagem de Jogadores')
-ax.set_ylabel('Posição Geral')
-ax.set_title('Posições dos jogadores')
-st.pyplot(fig)
-
-# Adicionando critério de desempate usando idade, potencial, hits e potencial máximo
-top_15_players = df[['name', 'overall', 'age', 'potential', 'hits']].nlargest(15, ['overall', 'age', 'potential', 'hits'])
-
-# ...
-
-# Restante do código (gráficos, tabelas, etc.)
