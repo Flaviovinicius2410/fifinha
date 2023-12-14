bc5d78a64e4d1c3947741496a19def0c76d94c7d
Flaviovinicius2410 <114126342+Flaviovinicius2410@users.noreply.github.com>
2023-12-13 23:20:46 -0300

    Add files via upload

--- pages/BaseDeDados.py
+++ pages/BaseDeDados.py
@@ -11,18 +11,16 @@ df = pd.read_csv("train.csv")
 
 # Fonte dos Dados
 st.write("## Fonte dos Dados")
-st.write("Os dados do Titanic foram obtidos do Kaggle, disponíveis no seguinte link:")
-st.write("[Titanic - Kaggle](https://www.kaggle.com/c/titanic/data)")
-
+st.write("JOGADORES DO FIFA")
 # Visão Geral
 st.write("## Visão Geral")
-st.write("O conjunto de dados do Titanic é amplamente utilizado para análises exploratórias e modelagem preditiva.")
-st.write("Ele contém informações sobre os passageiros a bordo do Titanic, incluindo detalhes sobre sobrevivência, classe, sexo, idade, número de irmãos/cônjuges a bordo, número de pais/filhos a bordo, tarifa, entre outros.")
+st.write("Escolhi essa base de dados do ano de 2021 para estudar os dados que fazem um jogador ser considerado um dos melhores do munndo quanto ourtro um dos piores")
+st.write("Essa base tem como informação o overall atual dos jogadores, seu possivel potencial junto a idade e hits de cada jogador")
 
 # Estrutura do Conjunto de Dados
 st.write("## Estrutura do Conjunto de Dados")
 st.write(f"- **Número de Linhas (Amostras):** {df.shape[0]}")
-st.write(f"- **Número de Colunas (Variáveis):** {df.shape[1]}")
+st.write(f"- **Número de Colunas (Variáveis):** {df.shape[11]}")
 
 # Colunas Principais
 st.write("## Colunas Principais")
@@ -35,24 +33,16 @@ st.write("## Análise Exploratória")
 # Display an interactive data table
 st.write("### Visualização da Tabela de Dados")
 st.dataframe(df)
+df = pd.read_csv("train.csv", delimiter=";")
 
-# Display a bar chart showing the distribution of passengers by survival status
-st.write("### Distribuição de Passageiros por Sobrevivência")
-survival_chart = alt.Chart(df).mark_bar().encode(
-    x='Survived:O',
+# Gráfico de distribuição de jogadores por pontuação "Overall"
+st.write("### Distribuição de Jogadores por Pontuação Overall")
+overall_chart = alt.Chart(df).mark_bar().encode(
+    x='overall:O',
     y='count()',
-    color='Survived:N'
+    color='overall:N'
 ).properties(
-    title="Distribuição de Passageiros por Sobrevivência",
+    title="Distribuição de Jogadores por Pontuação Overall",
     width=600
 )
-st.altair_chart(survival_chart, use_container_width=True)
-
-# Display a link to the pandas profiling report
-st.write("### Link para o Relatório de Perfil do Conjunto de Dados")
-st.markdown("[Download do Relatório de Perfil](sandbox:/path/to/train.csv)")
-
-# Conclusão
-st.write("## Conclusão")
-st.write("Este documento fornece uma visão geral do conjunto de dados do Titanic, incluindo suas fontes, estrutura e principais colunas.")
-st.write("A análise exploratória detalhada, incluindo visualizações interativas, pode revelar insights valiosos sobre os eventos que ocorreram a bordo do Titanic.")
+st.altair_chart(overall_chart, use_container_width=True)
