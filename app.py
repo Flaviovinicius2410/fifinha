ee92f874a6e375eabfa0314acd8d340a1a2cfc6a
Flaviovinicius2410 <114126342+Flaviovinicius2410@users.noreply.github.com>
2023-12-15 14:24:58 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,10 +7,32 @@ import numpy as np
 # Usando ";" como delimitador no read_csv
 df = pd.read_csv("train.csv", delimiter=";")
 
+# Imprima as colunas do DataFrame
+st.write("Colunas no DataFrame:", df.columns)
+
+# Informações sobre jogadores
+st.subheader("Informações sobre Jogadores:")
+# Exibe uma tabela com informações específicas sobre os jogadores
+st.write(df[['player_id', 'name', 'nationality', 'position', 'overall', 'age', 'hits', 'potential', 'team']])
+
+# Filtrar apenas as posições puras
+# Conta a quantidade de jogadores por posição pura
+pure_positions = df['position'].str.split('|', expand=True).stack().value_counts()
+
+# Gráfico de distribuição de jogadores por posição pura em um gráfico de barras
+st.subheader("Jogadores Por Posições:")
+# Cria um gráfico de barras horizontais mostrando a distribuição de jogadores por posição pura
+fig, ax = plt.subplots()
+pure_positions.sort_values().plot(kind='barh', ax=ax, color='skyblue')
+ax.set_xlabel('Contagem de Jogadores')
+ax.set_ylabel('Posição Geral')
+ax.set_title('Posições dos jogadores')
+st.pyplot(fig)
+
 # Adicionando critério de desempate usando idade, potencial, hits e potencial máximo
 top_15_players = df[['name', 'overall', 'age', 'potential', 'hits']].nlargest(15, ['overall', 'age', 'potential', 'hits'])
 
-# Calculando o potencial atual, potencial máximo, a diferença de potencial e a idade invertida
+# Calculando o potencial atual, potencial máximo e a diferença de potencial
 current_potential = top_15_players['potential']
 max_potential = top_15_players.groupby('name')['potential'].transform('max')
 potential_diff = current_potential - top_15_players['overall']
@@ -22,24 +44,34 @@ colors = np.where(potential_diff >= 0, 'green', 'orange')
 # Ordenando a tabela dos melhores 15 jogadores do maior para o menor
 top_15_players = top_15_players.sort_values(by=['overall', 'age', 'potential', 'hits'], ascending=[False, True, False, False])
 
-# Gráfico dos 15 melhores jogadores usando o critério de desempate
-fig, (ax_overall, ax_diff_potential) = plt.subplots(1, 2, figsize=(12, 6))
+# Gráfico de barras para os 15 melhores jogadores
+bar_width = 0.5
+bar_spacing = 1  # Espaçamento entre as barras
+names_with_spacing = top_15_players['name'] + ' ' + (np.arange(len(top_15_players)) * bar_spacing).astype(str)
 
-# Gráfico de barras para Overall
-ax_overall.barh(top_15_players['name'], top_15_players['overall'], color='green')
-ax_overall.set_ylabel('Jogadores')
-ax_overall.set_xlabel('Pontuação Overall')
-ax_overall.set_title('Top 15 Jogadores - Overall')
-ax_overall.invert_yaxis()  # Inverte a ordem dos jogadores
+fig, ax = plt.subplots()
+ax.barh(names_with_spacing, top_15_players['overall'], color='green', height=bar_width, label='Overall')
+ax.barh(names_with_spacing, potential_diff, left=top_15_players['overall'].min(), color=colors, height=bar_width, alpha=0.5, label='Diferença de Potencial (não visível)')
 
-# Gráfico de barras para Diferença de Potencial
-ax_diff_potential.barh(top_15_players['name'], potential_diff, color=colors)
-ax_diff_potential.set_ylabel('Jogadores')
-ax_diff_potential.set_xlabel('Diferença de Potencial')
-ax_diff_potential.set_title('Top 15 Jogadores - Diferença de Potencial')
-ax_diff_potential.invert_yaxis()  # Inverte a ordem dos jogadores
+ax.set_ylabel('Jogadores')
+ax.set_xlabel('Pontuação')
+ax.set_title('Top 15 Jogadores - Overall e Diferença de Potencial')
+ax.legend()
+ax.invert_yaxis()  # Inverte a ordem dos jogadores
 
-# Exibir os gráficos dos 15 melhores jogadores
+# Exibir tabela de Overall, Potencial Máximo, Diferença de Potencial e Idade Invertida
+st.write("### Tabela de Overall, Potencial Máximo, Diferença de Potencial (não visível) e Idade Invertida:")
+# Cria uma tabela com informações sobre os 15 melhores jogadores
+potentials_table = pd.DataFrame({
+    'Nome': top_15_players['name'],
+    'Overall': top_15_players['overall'],
+    'Potencial Máximo': max_potential,
+    'Diferença de Potencial (não visível)': potential_diff,
+    'Idade Invertida': inverse_age
+})
+st.write(potentials_table)
+
+# Mostrar o gráfico
 st.pyplot(fig)
 
 # Gráfico dos 10 melhores jogadores brasileiros
